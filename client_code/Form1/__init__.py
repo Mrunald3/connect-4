dcc892250b76073872b50f3c6b8590574f265a10
mrunal.dalvi@utexas.edu <mrunal.dalvi@utexas.edu>
2026-02-04 19:43:28 +0000

    Created Connect 4.jpg; Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,9 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.users
 
 
 class Form1(Form1Template):
@@ -7,4 +11,7 @@ class Form1(Form1Template):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
-    # Any code you write here will run before the form opens.
+    # Force the login pop-up to appear immediately
+    anvil.users.login_with_form() 
+
+    # Any code you write here will run after they successfully log in
